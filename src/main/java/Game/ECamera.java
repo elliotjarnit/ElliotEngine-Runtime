42fdc569b3944f57d9a6e4f8e639a0273a2a20a2
Swiftzerr <elliotjarnit@gmail.com>
2023-05-18 18:03:59 -0700

    camera!

---
+++ src/main/java/Game/ECamera.java
@@ -0,0 +1,46 @@
+package src.main.java.Game;
+
+import src.main.java.Utils.MathUtils.Matrix4;
+import src.main.java.Utils.MathUtils.Vector3;
+
+public class ECamera extends EEntity {
+    private double fov;
+
+    public ECamera() {
+        super();
+        this.fov = 90;
+    }
+    public ECamera(Vector3 origin) {
+        super(origin);
+        this.fov = 90;
+    }
+    public ECamera(Vector3 origin, double fov) {
+        super(origin);
+        this.fov = fov;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    public Matrix4 getViewMatrix() {
+        double cosPitch = (double) Math.cos(this.getRotation().x);
+        double sinPitch = (double) Math.sin(this.getRotation().x);
+        double cosYaw = (double) Math.cos(this.getRotation().y);
+        double sinYaw = (double) Math.sin(this.getRotation().y);
+
+        Vector3 xAxis = new Vector3(cosYaw, 0, -sinYaw);
+        Vector3 yAxis = new Vector3(sinYaw * sinPitch, cosPitch, cosYaw * sinPitch);
+        Vector3 zAxis = new Vector3(sinYaw * cosPitch, -sinPitch, cosPitch * cosYaw);
+
+        return new Matrix4(
+                new double[] {
+                        xAxis.x, yAxis.x, zAxis.x, 0,
+                        xAxis.y, yAxis.y, zAxis.y, 0,
+                        xAxis.z, yAxis.z, zAxis.z, 0,
+                        -xAxis.dot(this.getOrigin()), -yAxis.dot(this.getOrigin()), -zAxis.dot(this.getOrigin()), 1
+                }
+        );
+    }
+}
