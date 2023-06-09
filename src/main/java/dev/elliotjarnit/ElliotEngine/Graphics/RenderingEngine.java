c4251ecd84933ed0e48208b3a35bde693c40fba6
Swiftzerr <elliotjarnit@gmail.com>
2023-06-08 20:33:57 -0700

    why

--- src/main/java/dev/elliotjarnit/ElliotEngine/Graphics/RenderingEngine.java
+++ src/main/java/dev/elliotjarnit/ElliotEngine/Graphics/RenderingEngine.java
@@ -28,7 +28,7 @@ public class RenderingEngine extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.BLACK.toAwtColor());
+        g2d.setColor(scene.getSkyColor().toAwtColor());
         g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
 
         if (scene == null) return;
