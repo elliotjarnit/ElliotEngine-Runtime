b8f76cebcd0e75baa72dd174b5ef9fbf0b5ba17b
Swiftzerr <elliotjarnit@gmail.com>
2023-06-13 21:39:57 -0700

    decorations

--- src/main/java/dev/elliotjarnit/ElliotEngine/Overlay/EOText.java
+++ src/main/java/dev/elliotjarnit/ElliotEngine/Overlay/EOText.java
@@ -10,32 +10,50 @@ import java.awt.Font;
 public class EOText extends EOComponent {
     private String text;
     private int fontSize;
-    private EColor EColor;
+    private EColor color;
+    private EColor backgroundColor;
 
     public EOText(Vector2 position, String text) {
         super(position);
         this.text = text;
         this.fontSize = 12;
-        this.EColor = EColor.BLACK;
+        this.color = color.BLACK;
     }
 
-    public EOText(Vector2 position, String text, int fontSize, EColor EColor) {
+    public EOText(Vector2 position, String text, int fontSize, EColor color) {
         super(position);
         this.text = text;
         this.fontSize = fontSize;
-        this.EColor = EColor;
+        this.color = color;
+    }
+
+    public EOText(Vector2 position, String text, int fontSize, EColor color, EColor backgroundColor) {
+        super(position);
+        this.text = text;
+        this.fontSize = fontSize;
+        this.color = color;
+        this.backgroundColor = backgroundColor;
     }
 
     @Override
     public void render(Graphics2D g2d) {
-        g2d.setColor(this.EColor.toAwtColor());
         g2d.setFont(new Font("TimesRoman", Font.PLAIN, this.fontSize));
 
         int textWidth = g2d.getFontMetrics().stringWidth(this.text);
         int x = (int) this.getPosition().x - (textWidth / 2);
         int y = (int) this.getPosition().y;
 
+        if (this.backgroundColor != null) {
+            g2d.setColor(this.backgroundColor.toAwtColor());
+            g2d.fillRect(x - 4, y - this.fontSize - 4, textWidth + 8, this.fontSize + 8);
+        }
+
+        g2d.setColor(this.color.toAwtColor());
         g2d.drawString(this.text, x, y);
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 
     @Override
